a5f6ba66dd2041b52a05181b95dacf1187da7032
Maxim <mac28@tpu.ru>
2024-09-09 20:40:14 +0700

    feat(enum): add Categories and Difficulties enums

---
+++ src/main/java/backend/academy/hangman/configuration/Categories.java
@@ -0,0 +1,9 @@
+package backend.academy.hangman.configuration;
+
+public enum Categories {
+
+    COUNTRIES,
+    BRANDS,
+    MUSIC
+
+}
---
+++ src/main/java/backend/academy/hangman/configuration/Difficulties.java
@@ -0,0 +1,9 @@
+package backend.academy.hangman.configuration;
+
+public enum Difficulties {
+
+    EASY,
+    MEDIUM,
+    HARD,
+
+}
