3969af77aefde790d76907c14af1e17f7bba04a5
Maxim <mac28@tpu.ru>
2024-09-15 20:43:47 +0700

    feat(WordRandomizer): update constructor signature
    Update the constructor of `WordRandomizer` to add a new parameter `random`.

--- src/main/java/backend/academy/hangman/configuration/WordRandomizer.java
+++ src/main/java/backend/academy/hangman/configuration/WordRandomizer.java
@@ -4,12 +4,13 @@ import java.security.SecureRandom;
 import java.util.List;
 
 public class WordRandomizer {
-    private final SecureRandom random = new SecureRandom();
+    private final SecureRandom random;
     private final HangmanDictionary dictionary = HangmanDictionary.init();
 
     private final ConfigWord configWord;
 
-    public WordRandomizer(ConfigWord configWord) {
+    public WordRandomizer(SecureRandom random, ConfigWord configWord) {
+        this.random = random;
         this.configWord = configWord;
     }
 
