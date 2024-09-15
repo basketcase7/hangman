aa86f71493dce628ce0b0941ec874238f0a00564
Maxim <mac28@tpu.ru>
2024-09-15 17:34:29 +0700

    chore(imports): update imports in ConfigWord
    Updated import statement in `ConfigWord` class to reflect changes in package structure.

--- src/main/java/backend/academy/hangman/configuration/ConfigWord.java
+++ src/main/java/backend/academy/hangman/configuration/ConfigWord.java
@@ -1,5 +1,8 @@
 package backend.academy.hangman.configuration;
 
+import backend.academy.hangman.enums.Categories;
+import backend.academy.hangman.enums.Difficulties;
+
 public record ConfigWord(Categories categories, Difficulties difficulties) {
 
 }
