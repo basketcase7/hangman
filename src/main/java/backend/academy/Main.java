532683c4d806ff4c6a96ab31ea18e63b46f3f56d
Maxim <mac28@tpu.ru>
2024-09-16 17:02:15 +0700

    fix(main): preparing the Main class for launch

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,14 @@
 package backend.academy;
 
+import backend.academy.hangman.states.StateHandler;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        StateHandler stateHandler = new StateHandler();
+        stateHandler.start();
+
     }
 }
